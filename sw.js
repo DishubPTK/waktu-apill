c48ba546eaaf03bac7071ecef1735b57018e66dc
DishubPTK <pembangunandanops.dishubptk@gmail.com>
2026-04-23 07:42:30 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,8 @@
 // Service Worker for APILL PWA
+// Debug build: 2026-04-23 00:40
 // Updated: 2026-04-22 22:15:14
 // !! Update versi ini setiap kali ada file baru !!
-const CACHE_NAME = 'apill-v13';
+const CACHE_NAME = 'apill-v14';
 const urlsToCache = [
   '/waktu-apill/',
   '/waktu-apill/index.html',
