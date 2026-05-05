fa479e11b4dc1ce9341c59c5d1bb9dac9040fe2c
DishubPTK <pembangunandanops.dishubptk@gmail.com>
2026-05-05 21:24:34 +0700

    Add files via upload

--- password.js
+++ password.js
@@ -9,3 +9,5 @@ const PASSWORD_HASH  = 'da1f8a944fac13ec14aa3d95b81dfe1ac2f9a75e13b6ce7b86fde64b
 
 // PASSWORD2: untuk edit data & buka setup (rahasia, hanya admin)
 const PASSWORD2_HASH = 'b4b50bd8a86e78313909c8f6607a959068019ef0d28fe5535bf40b25f2938301';
+
+// 1beda490cc22b86b994fb33b23d25dd19eeaefefbc91eceede7394de313e43e1
