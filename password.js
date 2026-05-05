6790a0b71d9cea93dfa262af501d7dc9bc5ee3e3
DishubPTK <pembangunandanops.dishubptk@gmail.com>
2026-05-05 21:15:18 +0700

    Update password.js

--- password.js
+++ password.js
@@ -5,7 +5,7 @@
 // ═══════════════════════════════════════════════════════════
 
 // PASSWORD: untuk melihat data (bagikan ke semua yang butuh lihat data)
-const PASSWORD_HASH  = '1beda490cc22b86b994fb33b23d25dd19eeaefefbc91eceede7394de313e43e1';
+const PASSWORD_HASH  = 'da1f8a944fac13ec14aa3d95b81dfe1ac2f9a75e13b6ce7b86fde64b14884434';
 
 // PASSWORD2: untuk edit data & buka setup (rahasia, hanya admin)
 const PASSWORD2_HASH = 'b4b50bd8a86e78313909c8f6607a959068019ef0d28fe5535bf40b25f2938301';
